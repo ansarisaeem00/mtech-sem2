e3208817a69ac272ea8f372e2e986dd6d950af8d
ansarisaeem00 <ansarisaeem00@gmail.com>
2020-02-11 22:52:55 +0530

    sem2 initial commit

---
+++ seminar/ELA/ela-master/ela.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from PIL import Image, ImageChops
+
+
+#ORIG = './books-edited.jpg'
+ORIG = 'images.jpeg'
+TEMP = 'temp.jpg'
+SCALE = 10
+
+
+def ELA():
+    original = Image.open(ORIG)
+    original.save(TEMP, quality=90)
+    temporary = Image.open(TEMP)
+
+    diff = ImageChops.difference(original, temporary)
+    d = diff.load()
+    WIDTH, HEIGHT = diff.size
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            d[x, y] = tuple(k * SCALE for k in d[x, y])
+
+    diff.show()
+
+if __name__ == '__main__':
+    ELA()
