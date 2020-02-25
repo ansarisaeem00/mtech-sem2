43bdc3237cdd06462b182f6eeb1f2c3ea28e2713
ansarisaeem00 <ansarisaeem00@gmail.com>
2020-02-25 09:55:52 +0530

    nad expt7

--- Nad/Expts/7/des3.py
+++ Nad/Expts/7/des3.py
@@ -25,6 +25,30 @@ def encrypt(key, filename):
 
 		outfile.write(encryptor.encrypt(chunk))
 
+#Decryption of files
+def decrypt(key, filename):
+	chunksize =64 * 1024
+	outputFile = filename[11:]
+
+	with open(filename, 'rb') as infile:
+		filesize = int(infile.read(16))
+		IV = infile.read(16)
+
+	decryptor = AES.new(key, AES.MODE_CBC, IV)
+
+	with open(outputFile, 'wb') as outfile:
+		while True:
+			chunk = infile.read(chunksize)
+
+			if len(chunk) == 0:
+				break
+
+			outfile.write(decryptor.decrypt(chunk))
+			outfile.truncate(filesize)
+
+
+
+
 def getKey(password):
 	hasher = SHA256.new(password.encode('utf-8'))
 	return hasher.digest()
